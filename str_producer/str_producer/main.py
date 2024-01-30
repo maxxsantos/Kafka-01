8c22e378c98ec9105bf841779c9b6f2674d88d73
Max Simões dos Santos <max.santos@tpv-tech.com>
2024-01-30 18:10:36 -0400

    fix: add configs to define num partitions and replicas and add key in POST request

--- str_producer/str_producer/main.py
+++ str_producer/str_producer/main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI, HTTPException
 import uvicorn
 from confluent_kafka import Producer
 import json
+from confluent_kafka.admin import AdminClient, NewTopic
+
 
 kafka_bootstrap_servers = 'localhost:9092'
-kafka_topic = 'nome_do_topico'
+kafka_topic = 'teste'
+num_partitions = 3
+num_replicas = 2
 
 app = FastAPI()
 
@@ -31,22 +35,50 @@ def delivery_report(err, msg):
             )
 
 
-@app.post("/send")
-async def send_to_kafka(message: dict):
-    topic = 'teste'
+@app.post("/send/{key}")
+async def send_to_kafka(key: str, message: dict):
     try:
         # Enviar mensagem para o tópico especificado
         json_message = json.dumps(message)
-        producer.produce(topic, value=json_message, callback=delivery_report)
+
+        topic_config = {
+            'num_partitions': num_partitions,
+            'replication_factor': num_replicas
+        }
+
+        create_topic(kafka_bootstrap_servers, kafka_topic, topic_config)
+
+        producer.produce(
+            kafka_topic,
+            key=key,
+            value=json_message,
+            callback=delivery_report
+            )
         producer.flush()
 
         return {
             "status": "Mensagem enviada com sucesso para o tópico {}"
-            .format(topic)
+            .format(kafka_topic)
             }
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+def create_topic(bootstrap_servers, topic, config):
+
+    admin_config = {
+        'bootstrap.servers': bootstrap_servers,
+    }
+
+    admin = AdminClient(admin_config)
+
+    # Verifica se o tópico já existe
+    topics = admin.list_topics().topics
+    if topic not in topics:
+        # Criação do tópico com as configurações desejadas
+        new_topic = NewTopic(topic, **config)
+        admin.create_topics([new_topic])
 
 
 def start():
