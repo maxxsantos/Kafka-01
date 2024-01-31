56803847de661606b9e4a6043e7f300c45a0503a
Max Simões dos Santos <max.santos@tpv-tech.com>
2024-01-31 16:28:07 -0400

    update main

--- str_producer/str_producer/main.py
+++ str_producer/str_producer/main.py
@@ -3,11 +3,10 @@ from fastapi import FastAPI, HTTPException
 import uvicorn
 from confluent_kafka import Producer
 import json
-from confluent_kafka.admin import AdminClient, NewTopic
+import os
 
-
-kafka_bootstrap_servers = 'localhost:9092'
-kafka_topic = 'teste'
+kafka_bootstrap_servers = os.getenv('KAFKA_BROKER', 'localhost:9092')
+kafka_topic = os.getenv('KAFKA_TOPIC', 'teste1')
 num_partitions = 3
 num_replicas = 2
 
@@ -41,13 +40,6 @@ async def send_to_kafka(key: str, message: dict):
         # Enviar mensagem para o tópico especificado
         json_message = json.dumps(message)
 
-        topic_config = {
-            'num_partitions': num_partitions,
-            'replication_factor': num_replicas
-        }
-
-        create_topic(kafka_bootstrap_servers, kafka_topic, topic_config)
-
         producer.produce(
             kafka_topic,
             key=key,
@@ -65,22 +57,6 @@ async def send_to_kafka(key: str, message: dict):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-def create_topic(bootstrap_servers, topic, config):
-
-    admin_config = {
-        'bootstrap.servers': bootstrap_servers,
-    }
-
-    admin = AdminClient(admin_config)
-
-    # Verifica se o tópico já existe
-    topics = admin.list_topics().topics
-    if topic not in topics:
-        # Criação do tópico com as configurações desejadas
-        new_topic = NewTopic(topic, **config)
-        admin.create_topics([new_topic])
-
-
 def start():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -88,8 +64,8 @@ def start():
     config = uvicorn.Config(
         app=app,
         loop="auto",
-        port=3000,
-        host="localhost"
+        port=os.getenv('API_PORT', 3000),
+        host=os.getenv('API_HOST', 'localhost')
     )
     server = uvicorn.Server(config)
     loop.run_until_complete(server.serve())
